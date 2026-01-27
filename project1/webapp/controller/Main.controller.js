3d17818027bc83ae8bf76728ba62e0a668d2a0f1
veranikashneidzer <veranika.shneidzer@gmail.com>
2026-01-27 15:30:27 +0200

    Create empty project

---
+++ project1/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("project1.controller.Main", {
+        onInit() {
+        }
+    });
+});
