bfd8430bf6a653ec8b5e94e31ccf49c3c05cfcbd
veranikashneidzer <veranika.shneidzer@gmail.com>
2026-02-09 21:10:41 +0200

    [Task 5-2] New page

---
+++ project1/webapp/controller/Product.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+  "project1/controller/BaseController"
+], (BaseController,) => {
+  "use strict";
+
+  return BaseController.extend("project1.controller.Product", {
+    onInit() {}
+  })
+});
